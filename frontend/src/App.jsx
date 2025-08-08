ef51f9fcae514739c7ec60ea2d238ac0d7539501
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-08-08 12:59:25 +0600

    ** Project Setup Done **

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h2  className='text-6xl bg-amber-800' >Project Start</h2>
+    </div>
+  );
+};
+
+export default App;
