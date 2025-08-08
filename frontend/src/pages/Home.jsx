cff3d65e42754a0c1b0eede438756bd79771192e
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-08-09 03:00:09 +0600

    TrustNanner Done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
 import SpecialitySection from '../components/SpecialitySection';
+import TopDoctor from '../components/TopDoctor';
+import TrustedBanner from '../components/TrustedBanner';
 
 const Home = () => {
     return (
         <div>
         <Header/>
         <SpecialitySection/>
+        <TopDoctor/>
+        <TrustedBanner/>
         </div>
     );
 };
