b95e98d59210128190a875b3e2034ff1869567a1
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-08-10 00:26:59 +0600

    ABout us Done

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from "react";
+import contact from "../assets/assets_frontend/contact_image.png";
 
 const Contact = () => {
-    return (
+  return (
+    <div>
+      <div>
+        <p className="text-gray-900 text-center text-4xl mb-5">
+          Contact <span className="font-bold text-black">US</span>
+        </p>
+      </div>
+      <div className="flex justify-center items-center gap-15 ">
         <div>
-            Conatct Page
+          <img src={contact} alt="" />
         </div>
-    );
+        <div>
+          <h4>Our OFFICE</h4> <br />
+          <p>
+            54709 Willms Station Suite 350,
+            <br /> Washington, USA
+          </p> <br />
+          <p>
+            Tel: (415) 555‑0132 <br />
+            Email: greatstackdev@gmail.com
+          </p> <br />
+          <h4>Careers at PRESCRIPTO</h4>
+          <p>Learn more about our teams and job openings.</p> <br /> <br />
+          <button className="bg-[#5f6FFF] text-white px-8 py-3 rounded-full font-light hidden md:block cursor-pointer">
+            Explore Jobs
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- frontend/src/pages/Doctor.jsx
+++ frontend/src/pages/Doctor.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Doctor = () => {
     return (
         <div>
-            Doctor Page
+             <p className="text-gray-900 text-center text-4xl mb-5">
+          All <span className="font-bold text-black">Doctors</span>
+        </p>
         </div>
     );
 };
