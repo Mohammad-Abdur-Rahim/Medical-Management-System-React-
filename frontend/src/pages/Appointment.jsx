11fc43594383739fb93837026298673f505d8d78
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2025-08-16 19:48:35 +0600

    Time Slot Mange FN Done

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -5,8 +5,13 @@ import { assets } from "../assets/assets_frontend/assets";
 
 const Appointment = () => {
   const { docId } = useParams();
-  const { doctors,currencySymbol } = useContext(AppContext);
+  const { doctors, currencySymbol } = useContext(AppContext);
   const [docInfo, setDocInfo] = useState(null);
+  //   Slot Mange State -----------------------------------------
+  const [docSlots, setDocSlots] = useState([]);
+  const [slotIndex, setSlotIndex] = useState(0);
+  const [slotTime, setSlotTime] = useState("");
+  // **************************************  Slot Mange State -----------------------------------------*****************************
 
   useEffect(() => {
     const fetchDocInfo = () => {
@@ -17,6 +22,57 @@ const Appointment = () => {
 
     fetchDocInfo();
   }, [doctors, docId]);
+  // ****************************************  Slot Mange Fn -----------------------------------------
+  const getAvailableSlots = () => {
+  const slotsArray = [];
+  let today = new Date();
+
+  for (let i = 0; i < 7; i++) {
+    let currentDate = new Date(today);
+    currentDate.setDate(today.getDate() + i);
+
+    let endTime = new Date(today);
+    endTime.setDate(today.getDate() + i);
+    endTime.setHours(21, 0, 0, 0);
+
+    if (today.getDate() === currentDate.getDate()) {
+      currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10);
+      currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 10);
+    } else {
+      currentDate.setHours(10);
+      currentDate.setMinutes(0);
+    }
+
+    const timeSlot = [];
+    while (currentDate < endTime) {
+      const formattedTime = currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+      timeSlot.push({ datetime: new Date(currentDate), time: formattedTime });
+      currentDate.setMinutes(currentDate.getMinutes() + 30);
+    }
+
+    slotsArray.push(timeSlot);
+  }
+
+  setDocSlots(slotsArray); 
+};
+
+  //   Slot Mange State -----------------------------------------*****************************************
+
+  useEffect(()=>{
+   getAvailableSlots()
+  },[docInfo])
+
+useEffect(() => {
+  console.log(docSlots);
+}, [docSlots]);
+
+
+    //   Slot Mange Fn -----------------------------------------**************************************************
+
+
+
+
+
 
   return (
     docInfo && (
@@ -36,20 +92,31 @@ const Appointment = () => {
               {docInfo.name}
               <img className="w-5" src={assets.verified_icon} alt="" />
             </p>
-          
-          <div className="flex items-center gap-2 text-sm mt-1 text-gray-600">
-            <p>
-              {docInfo.degree} - {docInfo.speciality}
+
+            <div className="flex items-center gap-2 text-sm mt-1 text-gray-600">
+              <p>
+                {docInfo.degree} - {docInfo.speciality}
+              </p>
+              <button className="py-0.5 px-2 border text-xs rounded-full">
+                {docInfo.experience}
+              </button>
+            </div>
+            <div>
+              <p className="flex item-center gap-1 text-sm font-medium text-gray-900 mt-3">
+                About <img src={assets.info_icon} alt="" />
+              </p>
+              <p className="text-sm text-gray-500 max-w-[700px] mt-1">
+                {docInfo.about}
+              </p>
+            </div>
+            <p className="text-gray-500 font-medium mt-4">
+              Appointment Fee :{" "}
+              <span className="text-gray-600">
+                {currencySymbol}
+                {docInfo.fees}
+              </span>{" "}
+              Dollar
             </p>
-            <button className="py-0.5 px-2 border text-xs rounded-full">{docInfo.experience}</button>
-          </div>
-          <div>
-            <p className="flex item-center gap-1 text-sm font-medium text-gray-900 mt-3">
-              About <img src={assets.info_icon} alt="" />
-            </p>
-            <p className="text-sm text-gray-500 max-w-[700px] mt-1">{docInfo.about}</p>
-          </div>
-          <p className="text-gray-500 font-medium mt-4">Appointment Fee :  <span className="text-gray-600">{currencySymbol}{docInfo.fees}</span> Dollar</p>
           </div>
         </div>
       </div>
